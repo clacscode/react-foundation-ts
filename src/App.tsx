cc183d80c313b08dc63f6e11001e0e981e1d2827
Claudio S <clacscode@Claudios-MacBook-Pro.local>
2024-02-07 19:50:52 -0300

    React foundation with vite ts react-dom react-hook-form and zustand

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Counter, CounterWithHook, FormsPage, LoginPage, UserPage } from './components';
+
+function App() {
+
+  return (
+    <main>
+      <h1>Introducción a TS - React</h1>
+
+      {/* <Counter /> */}
+      {/* <CounterWithHook /> */}
+      {/* <LoginPage /> */}
+      {/* <UserPage /> */}
+      <FormsPage />
+
+
+    </main>
+  )
+}
+
+export default App
